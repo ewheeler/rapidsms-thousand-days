37f0c90ec724eb721006bcdd13cf626ccec064e2
ewheeler <evanmwheeler@gmail.com>
2013-07-18 13:56:29 -0400

    set django settings in raven hook

--- thousand/hooks.py
+++ thousand/hooks.py
@@ -1,4 +1,6 @@
 def run_raven(watcher, arbiter, hook_name):
+    import os
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", watcher.env["DJANGO_SETTINGS_MODULE"])
     from django.core.management import call_command
     call_command('validate')
     return True
