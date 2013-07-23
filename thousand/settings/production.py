65f8f8914a3e813c6a74e25717a533882f4ca08c
ewheeler <evanmwheeler@gmail.com>
2013-07-23 09:36:32 -0400

    remove duplicate setting

--- thousand/settings/production.py
+++ thousand/settings/production.py
@@ -9,6 +9,3 @@ EMAIL_SUBJECT_PREFIX = '[Thousand Prod] '
 
 LOGGING['handlers']['file']['level'] = 'INFO'
 LOGGING['handlers']['file']['filename'] = '/var/log/thousand/rapidsms.log'
-
-# TODO set this via salt and/or fabric
-CLEAVER_DATABASE = 'sqlite:////home/ewheeler/www/staging/thousand/experiments/experiment.data'
