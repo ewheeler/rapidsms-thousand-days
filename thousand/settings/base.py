626e8be6cc45dd6f98a6a6eda7b6d02c47b4ef62
ewheeler <evanmwheeler@gmail.com>
2013-08-21 13:16:53 -0400

    remove extra space

--- thousand/settings/base.py
+++ thousand/settings/base.py
@@ -49,7 +49,7 @@ LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-ALLOWED_HOSTS = [ '.localhost', ]
+ALLOWED_HOSTS = ['.localhost', ]
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
