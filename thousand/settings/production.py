199215c058f7a2b5cf668642a3ee19a16504785d
ewheeler <evanmwheeler@gmail.com>
2014-02-18 17:25:54 +0300

    update productions settings

--- thousand/settings/production.py
+++ thousand/settings/production.py
@@ -1,5 +1,17 @@
 from thousand.settings.staging import *
 
+TIME_ZONE = 'Africa/Kampala'
+LANGUAGE_CODE = 'en-UG'
+COUNTRY_CODES = ('UG',)
+
+# disable L10N because django does not format
+# dates appropriately for 'en-UG'
+USE_L10N = False
+DATE_FORMAT = 'j N Y'
+DATETIME_FORMAT = 'H:i T j N Y'
+SHORT_DATE_FORMAT = 'd/m/Y'
+SHORT_DATETIME_FORMAT = 'H:i d/m/Y'
+
 # There should be only minor differences from staging
 ALLOWED_HOSTS = ['.thousand-days.lobos.biz',
                  '.omega.unicefuganda.org',
@@ -20,3 +32,5 @@ INSTALLED_BACKENDS = {
         "encode_errors": "ignore",
     },
 }
+
+LOGGING['handlers']['rapidsms_file']['filename'] = '/var/www/thousand-production/log/rapidsms-router.log'
